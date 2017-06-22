ed1d6491c26cfd8c5e3a53b6a4e10d33b6a51608
Adam Silver <adampaulsilver@gmail.com>
2017-06-22 01:59:51 -0400

    gave up

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Post
 
-def post(request):
+def post(request ):
     testvar = "TEST VARIABLE PLZ IGNORE"
-    return render(request, 'posts/main.html', {'testvar': testvar})
+    post = Post.objects.all()
+    
+    return render(request, 'posts/main.html',\
+            {'testvar': testvar}, {'post':post}
+    )
