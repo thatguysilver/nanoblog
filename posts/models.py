c79846a4ced85973b8f1506ecc81a49015eebea4
Adam Silver <adampaulsilver@gmail.com>
2017-06-23 03:54:55 -0400

    basic working version completed.

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     content = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField('date published', null = True)
 
     def __str__(self):
         return self.content
--- posts/views.py
+++ posts/views.py
@@ -6,9 +6,10 @@ from .models import Post
 def post(request ):
     #testvar = "TEST VARIABLE PLZ IGNORE"
     post_list = Post.objects.order_by('id')
+    pub_date = Post.pub_date
     
     return render(request, 'posts/main.html',
-            {'post': post_list}, 
+            {'post': post_list, 'pub_date' : pub_date}, 
     )
 
 def content_get(request):
