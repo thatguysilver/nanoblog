251c080c878ff84a5c5d5a06d6ddb73b7d2bb969
Adam Silver <adampaulsilver@gmail.com>
2017-06-23 20:29:51 -0400

    created new model for lists

--- posts/models.py
+++ posts/models.py
@@ -6,3 +6,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.content
+
+class List(models.Model):
+    content = models.CharField(max_length = 100)
+    pub_date = models.DateTimeField('date published', auto_now = True, null = True)
+
+    def __str__(self):
+        return self.content
+
