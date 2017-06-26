f0bb5dec020f35f40e8f43e027a1a162726ffead
thatguysilver <adampaulsilver@gmail.com>
2017-06-26 07:40:54 +0000

    got picture to show up

--- lists/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class List(models.Model):
-    content = models.CharField(max_length = 100)
-    pub_date = models.DateTimeField('date published', auto_now = True, null = True)
-
-    def __str__(self):
-        return self.content
